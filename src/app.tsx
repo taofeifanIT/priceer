edc81ed81a68f0320fe072d324515779e18e7a83
taofeifanIT <3553447302@qq.com>
2022-06-30 13:48:56 +0800

    change error msg

--- src/app.tsx
+++ src/app.tsx
@@ -145,22 +145,22 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
 };
 
 const codeMessage = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  405: '请求方法不被允许。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。',
+  200: 'The server successfully returned requested data.  ',
+  201: 'Data creation or modification succeeded.  ',
+  202: 'A request has been queued in the background (asynchronous task).  ',
+  204: 'Data deleted successfully.  ',
+  400: 'An error occurred in the request. The server did not create or modify data.  ',
+  401: 'User has no permissions (wrong token, username, password).  ',
+  403: 'The user is authorized, but access is forbidden.  ',
+  404: 'The request was made for a nonexistent record, no action was taken by the server.  ',
+  405: 'Request method not allowed.  ',
+  406: 'Requested format not available.  ',
+  410: 'The requested resource is permanently deleted and will not be retrieved.  ',
+  422: 'A validation error occurred while creating an object.  ',
+  500: 'Server error, please check server.  ',
+  502: 'Gateway error.  ',
+  503: 'Service unavailable, server temporarily overloaded or maintained.  ',
+  504: 'Gateway timed out.  ',
 };
 
 /** 异常处理程序
@@ -182,8 +182,8 @@ const errorHandler = (error: ResponseError) => {
 
   if (!response) {
     notification.error({
-      description: '您的网络发生异常，无法连接服务器',
-      message: '网络异常',
+      description: 'Your network is abnormal and you cannot connect to the server',
+      message: 'network anomaly',
     });
     // setTimeout(() => {
     //   history.push('/user/login');
